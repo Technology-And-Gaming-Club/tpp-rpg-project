93514eaa73c6a6b5904c307da091c67e57b4e9f6
Darsh Kumar <darsh.kumar2002@gmail.com>
2022-06-11 18:08:04 +0530

    Cleaned Up Code Aded sprint functionality

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterControllerScript : MonoBehaviour {
 	[Header("Movement")]
 	public bool isGrounded;
+	public float sprintSpeed;
 	public float runSpeed;
 	public float walkSpeed;
 	[Range(0f, 0.5f)]
@@ -21,15 +22,18 @@ public class CharacterControllerScript : MonoBehaviour {
 	public float mouseXSens;
 	public float mouseYSens;
 
-	[Header("Inputs")]
+	[Header("Analogue Inputs")]
 	[Range(-1f, 1f)]
 	public float moveHorInput;
 	[Range(-1f, 1f)]
 	public float moveVertInput;
 	[Range(-1f, 1f)]
-	public float mouseXInput;
+	public float mouseHorInput;
 	[Range(-1f, 1f)]
-	public float mouseYInput;
+	public float mouseVertInput;
+
+	[Header("Digital Inputs")]
+	public bool sprint;
 	public bool walk;
 	public bool jump;
 	public bool crouch;
@@ -37,10 +41,8 @@ public class CharacterControllerScript : MonoBehaviour {
 	[Header("Misc")]
 	public Rigidbody rb;
 	public CharacterController cc;
-	public Transform camera;
-	public Transform body;
+	public Transform cam;
 	public Transform head;
-	public float groundDistance;
 
 
 	void Start() {
@@ -54,6 +56,7 @@ public class CharacterControllerScript : MonoBehaviour {
 	void Update() {
 		isGrounded = cc.isGrounded;
 		getInputVals();
+		setMoveSpeed();
 		mouseMovement();
 		playerMovement();
 		miscControlFunctions();
@@ -64,46 +67,25 @@ public class CharacterControllerScript : MonoBehaviour {
 	void getInputVals() {
 		moveHorInput = Input.GetAxis("Horizontal");
 		moveVertInput = Input.GetAxis("Vertical");
-		mouseXInput = Input.GetAxis("Mouse X");
-		mouseYInput = Input.GetAxis("Mouse Y");
+		mouseHorInput = Input.GetAxis("Mouse X");
+		mouseVertInput = Input.GetAxis("Mouse Y");
+
+		sprint = Input.GetButton("Sprint");
 		walk = Input.GetButton("Walk");
 		jump = Input.GetButton("Jump");
 		crouch = Input.GetButton("Crouch");
-
-		mouseX = transform.rotation.eulerAngles.y;
-		mouseY = -camera.rotation.eulerAngles.x;
 	}
-	
-	void mouseMovement() {
-		float mouseXDel = mouseXSens * mouseXInput * Time.deltaTime;
-		float mouseYDel = mouseYSens * mouseYInput * Time.deltaTime;
 
-		mouseX += mouseXDel;
-		mouseY += mouseYDel;
-
-		if(mouseX < 0) {
-			mouseX += 360;
-		} else if(mouseX > 360f) {
-			mouseX -= 360;
-		}
-		if(mouseY < -35 && mouseY > -265) {
-			mouseY = -35;
-		} else if(mouseY > -270 && mouseY < -265) {
-			mouseY = -270;
-		}
-
-		Vector3 finalPlayerRotation = new Vector3(0, mouseX, 0);
-		Vector3 finalCamRotation = new Vector3(-mouseY, 0, 0);
-
-		transform.rotation = Quaternion.Euler(finalPlayerRotation);
-		camera.localRotation = Quaternion.Euler(finalCamRotation);
-	}
-	void playerMovement() {
-		if(walk) {
+	void setMoveSpeed() {
+		if(sprint) {
+			moveSpeed = sprintSpeed;
+		} else if(walk) {
 			moveSpeed = walkSpeed;
 		} else {
 			moveSpeed = runSpeed;
 		}
+	}
+	void playerMovement() {
 		Vector3 moveDir;
 		moveDir = moveHorInput * transform.right + moveVertInput * transform.forward;
 		targetVelocity = moveSpeed * moveDir;
@@ -128,6 +110,41 @@ public class CharacterControllerScript : MonoBehaviour {
 		} else {
 			transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1f, 1f, 1f), 0.5f);
 			head.localScale = new Vector3(0.4f, 0.4f / transform.localScale.y, 0.4f);
+		}
+	}
+
+	void mouseMovement() {
+		mouseX += mouseHorInput * sens;
+		mouseY += mouseVertInput * sens;
+
+		clampMouseValues();
+		Vector3 camRotation;
+		Vector3 playerRotation;
+		convertMopuseToRotation(out camRotation, out playerRotation);
+		cam.localRotation = Quaternion.Euler(camRotation);
+		transform.rotation = Quaternion.Euler(playerRotation);
+	}
+	void convertMopuseToRotation(out Vector3 camRotation, out Vector3 playerRotation) {
+		camRotation = new Vector3();
+		playerRotation = new Vector3();
+		playerRotation.y = mouseX;
+		if(mouseY <= 0) {
+			camRotation.x = -mouseY;
+		} else {
+			camRotation.x = 360 - mouseX;
+		}
+	}
+	void clampMouseValues() {
+		if(mouseX > 360) {
+			mouseX -= 360;
+		} else if(mouseX < 0) {
+			mouseX += 360;
+		}
+
+		if(mouseY > 90) {
+			mouseY = 90;
+		} else if(mouseY < -40) {
+			mouseY = -40;
 		}
 	}
 }
