025213e6f5a399a961a0dc64f8ab5c98c602d34d
Darsh Kumar <darsh.kumar2002@gmail.com>
2022-07-07 18:43:24 +0530

    Aded Mission Interactability

---
+++ Assets/Scripts/Mission Interactables/PositionTriggerBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Mission_Interactables {
+	public class PositionTriggerBehaviour : MonoBehaviour {
+
+		// Use this for initialization
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
