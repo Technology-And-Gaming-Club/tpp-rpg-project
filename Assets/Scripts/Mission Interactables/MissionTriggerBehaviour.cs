8a35eb61b549c68633bad6510564d230753d398c
Darsh Kumar <darsh.kumar2002@gmail.com>
2022-07-08 19:49:37 +0530

    Updated Mission Start Trigger

--- Assets/Scripts/Mission Interactables/MissionTriggerBehaviour.cs
+++ Assets/Scripts/Mission Interactables/MissionTriggerBehaviour.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 public class MissionTriggerBehaviour : MonoBehaviour {
 	public GameObject targetMission;
 	public GameObject targetPlayer;
+	public float markerRadius;
+	float playerDist;
 
 	void Update() {
-		
+		pollPlayerDist();
+
+		if(playerDist < markerRadius) {
+			targetMission.SendMessage("activateMission", null, SendMessageOptions.DontRequireReceiver);
+			Destroy(gameObject);
+		}
 	}
 
 	void pollPlayerDist() {
 		Vector3 playerPos, pos;
-		RaycastHit hit;
-	}
+		playerPos = targetPlayer.transform.position - Vector3.up * targetPlayer.transform.position.y;
+		pos = transform.position - Vector3.up * transform.position.y;
 
-	void activateMission() {
-		targetMission.SendMessage("activateMission", null, SendMessageOptions.DontRequireReceiver);
-		Destroy(gameObject);
-	}
-
-	void OnCollisionEnter(Collision collision) {
-		if(collision.rigidbody.gameObject == targetPlayer) {
-			targetMission.SendMessage("targetMarkerActivated", null, SendMessageOptions.DontRequireReceiver);
-			Destroy(gameObject);
-		}
-	}
-
-	void OnCollisionStay(Collision collision) {
-		if(collision.rigidbody.gameObject == targetPlayer) {
-			targetMission.SendMessage("targetMarkerActivated", null, SendMessageOptions.DontRequireReceiver);
-			Destroy(gameObject);
-		}
+		playerDist = (playerPos - pos).magnitude;
 	}
 }
