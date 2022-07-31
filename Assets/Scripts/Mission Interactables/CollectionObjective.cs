7034391e4f33862136c331e844e8c4c2dc4a8308
Darsh Kumar <darsh.kumar2002@gmail.com>
2022-07-31 11:01:42 +0530

    Modified Mission Objective Structures

---
+++ Assets/Scripts/Mission Interactables/CollectionObjective.cs
@@ -0,0 +1,24 @@
+﻿/*
+	Will be done once Srivishnu's object collection and inventory system is implemented and committed
+*/
+
+using System.Collections;
+using UnityEngine;
+
+public class CollectionObjective : MissionObjective {
+
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	public override void play() {
+		
+	}
+}
